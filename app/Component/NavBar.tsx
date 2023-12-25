668e651574e995b74b1fb6250b591c50c5e5a1b2
ayowaaab <ayoub.dahmen@esprit.tn>
2023-12-25 16:04:11 +0100

    set up prisma

--- app/Component/NavBar.tsx
+++ app/Component/NavBar.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { IoLogoIonitron } from "react-icons/io";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  const currentPath = usePathname();
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="flex items-center space-x-5 border-b-2 mb-5 h-14 px-5 ">
+      <Link href="/">
+        <IoLogoIonitron />
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-gray-500": link.href !== currentPath,
+              "text-gray-900": link.href === currentPath,
+              "hover:text-gray-900 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
