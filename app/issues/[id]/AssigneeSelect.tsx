1d9e505da32d0b0e6a6e8ae29a16a82e88417a9d
ayowaaab <ayoub.dahmen@esprit.tn>
2024-07-10 19:34:13 +0100

    ✅ Fetch Data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,7 +3,7 @@ import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
   const {
@@ -13,8 +13,11 @@ const AssigneeSelect = () => {
   } = useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime:60 * 1000
+    staleTime: 60 * 1000, //60s
+    retry: 3,
   });
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
 
   return (
     <Select.Root>
