7f7d85f15260996dc0e3973f30595b5586d24f55
ayowaaab <ayoub.dahmen@esprit.tn>
2024-07-10 20:39:54 +0100

    ✅ Implementing the API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/db";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
@@ -7,15 +7,23 @@ interface IParams {
   params: { id: string };
 }
 export async function PATCH(request: Request, { params: { id } }: IParams) {
-  const session = await getServerSession();
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession();
+  // if (!session) return NextResponse.json({}, { status: 401 });
   try {
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
       return NextResponse.json(validation.error.format(), { status: 400 });
 
-    const { title, description } = body;
+    const { title, description, assignedToUserId } = body;
+    if (assignedToUserId) {
+      const user = await prisma.user.findUnique({
+        where: { id: assignedToUserId },
+      });
+      if (!user)
+        return NextResponse.json({ error: "Invalid User." }, { status: 400 });
+
+    }
     const issue = await prisma.issue.findUnique({
       where: { id: parseInt(id) },
     });
@@ -24,8 +32,9 @@ export async function PATCH(request: Request, { params: { id } }: IParams) {
     const updateIssue = await prisma.issue.update({
       where: { id: parseInt(id) },
       data: {
-        title: title,
-        description: description,
+        title ,
+        description,
+        assignedToUserId
       },
     });
     return NextResponse.json(updateIssue, { status: 201 });
